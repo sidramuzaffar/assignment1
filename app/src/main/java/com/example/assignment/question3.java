2bb2794380ea474087e1f37ab29cae71cbd20f36
sidramuzaffar <sidramuzaffar8452@gmail.com>
2023-09-05 22:44:43 +0500

    commit

---
+++ app/src/main/java/com/example/assignment/question3.java
@@ -0,0 +1,14 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class question3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question3);
+    }
+}
